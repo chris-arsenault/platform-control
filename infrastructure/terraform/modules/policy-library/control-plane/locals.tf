bff058bbc5422463c29dc96853affdde3fa84681
Chris Arsenault <carsena2@gmail.com>
2025-10-13 22:03:00 -0400

    change prefixing

--- infrastructure/terraform/modules/policy-library/control-plane/locals.tf
+++ infrastructure/terraform/modules/policy-library/control-plane/locals.tf
@@ -1,9 +1,7 @@
 
 locals {
   deployment_role_name                         = "deployer-${var.prefix}"
-  project_guardrails_permissions_boundary_name = "pb-${var.prefix}-project-guardrails"
-  project_boundary_arn = "arn:aws:iam::${var.account_id}:policy/${local.project_guardrails_permissions_boundary_name}"
-
+  project_boundary_arn = "arn:aws:iam::${var.account_id}:policy/pb-${var.prefix}-project-guardrails"
   # Common ARNs
   prefixed_roles_arn       = "arn:aws:iam::${var.account_id}:role/deployer-*"
   permissions_boundary_arn = "arn:aws:iam::${var.account_id}:policy/pb-*"
