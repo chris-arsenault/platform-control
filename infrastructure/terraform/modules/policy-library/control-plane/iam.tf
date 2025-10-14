d47a270b9beb8b6d5a10cddc24262a78cdb45382
Chris Arsenault <carsena2@gmail.com>
2025-10-13 20:44:44 -0400

    remove *

--- infrastructure/terraform/modules/policy-library/control-plane/iam.tf
+++ infrastructure/terraform/modules/policy-library/control-plane/iam.tf
@@ -134,7 +134,7 @@ data "aws_iam_policy_document" "this" {
       "iam:TagPolicy",
       "iam:DeletePolicyVersion"
     ]
-    resources = [local.prefixed_policies_arn, "*"]
+    resources = [local.prefixed_policies_arn]
   }
 
 }
