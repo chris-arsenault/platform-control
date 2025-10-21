80a53f919dc35a09517eadcc7253ace68ca59aed
Chris Arsenault <carsena2@gmail.com>
2025-10-21 10:19:13 -0700

    allow eni creation

--- infrastructure/terraform/modules/policy-library/vpn/iam.tf
+++ infrastructure/terraform/modules/policy-library/vpn/iam.tf
@@ -43,6 +43,7 @@ data "aws_iam_policy_document" "this" {
     actions = [
       "ec2:AttachInternetGateway",
       "ec2:CreateRoute",            # route-table + target must be tagged
+      "ec2:CreateNetworkInterface",
       "ec2:AssociateRouteTable",    # route-table + subnet must be tagged
       "ec2:DeleteVpc",
       "ec2:DeleteSubnet",
