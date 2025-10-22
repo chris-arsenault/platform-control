4ff733cd8a6cc23bdb07e993d941f43693503c08
Chris Arsenault <carsena2@gmail.com>
2025-10-21 17:44:50 -0700

    add to policy map

--- infrastructure/terraform/modules/managed-project/policy-map.tf
+++ infrastructure/terraform/modules/managed-project/policy-map.tf
@@ -54,6 +54,7 @@ locals {
     "control-plane"  = module.control-plane.policy_json
     "iam"            = module.iam.policy_json
     "state"          = module.state.policy_json
+    "reverse-proxy"  = module.reverse-proxy.policy_json
     "static-website" = module.static-website.policy_json
     "vpn"            = module.vpn.policy_json
   }
