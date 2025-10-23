6bd0147cb5334a32a3d323028069197d9797dddb
Chris Arsenault <carsena2@gmail.com>
2025-10-23 02:26:46 -0700

    asg setup

--- infrastructure/terraform/modules/policy-library/vpn/iam.tf
+++ infrastructure/terraform/modules/policy-library/vpn/iam.tf
@@ -19,6 +19,7 @@ data "aws_iam_policy_document" "this" {
     sid = "AllowAutoscaling"
     actions = [
       "autoscaling:CreateAutoScalingGroup",
+      "autoscaling:UpdateAutoScalingGroup"
     ]
     resources = ["*"]
   }
